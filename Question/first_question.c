8a396f606628fd1812b64b37e040883c64d9cfc7
xinon83323 <aliamjad83323@gmail.com>
2024-06-15 01:23:40 +0530

    add .exe file

---
+++ Question/first_question.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ( ) {
+    int first_number , second_number ;
+    printf(" Enter Your First Number \n");
+    scanf("%d",&first_number);
+    printf(" Enter Your Second Number \n");
+    scanf("%d",&second_number);
+    printf(" Sum is : %d", first_number + second_number) ;
+    return 0;
+}
